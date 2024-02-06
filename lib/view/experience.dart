1d3507a293a61a9018fce6adf1c0eada9afc695b
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-06 16:14:31 +0100

    ModifiedM lib/view/experience.dart M  pubspec.yaml

--- lib/view/experience.dart
+++ lib/view/experience.dart
@@ -1,4 +1,8 @@
+import 'package:comment_tree/comment_tree.dart';
+import 'package:easy_stepper/easy_stepper.dart';
 import 'package:flutter/material.dart';
+
+import '../shared/globals.dart';
 
 class Experience extends StatefulWidget {
   const Experience({super.key});
@@ -10,6 +14,80 @@ class Experience extends StatefulWidget {
 class _ExperienceState extends State<Experience> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: oddDarkBgColor,
+      alignment: Alignment.center,
+      height: MediaQuery.sizeOf(context).height * .2,
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      child: EasyStepper(
+        activeStep: 0,
+        activeStepTextColor: blueColor,
+        finishedStepTextColor: whiteColor,
+        internalPadding: 0,
+        showLoadingAnimation: false,
+        stepRadius: 8,
+        showStepBorder: false,
+        steps: <EasyStep>[
+          EasyStep(
+            customStep: CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: activeStep >= 0 ? Colors.orange : Colors.white,
+              ),
+            ),
+            customS: 'Waiting',
+          ),
+          EasyStep(
+            customStep: CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: activeStep >= 1 ? Colors.orange : Colors.white,
+              ),
+            ),
+            title: 'Order Received',
+            topTitle: true,
+          ),
+          EasyStep(
+            customStep: CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: activeStep >= 2 ? Colors.orange : Colors.white,
+              ),
+            ),
+            title: 'Preparing',
+          ),
+          EasyStep(
+            customStep: CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: activeStep >= 3 ? Colors.orange : Colors.white,
+              ),
+            ),
+            title: 'On Way',
+            topTitle: true,
+          ),
+          EasyStep(
+            customStep: CircleAvatar(
+              radius: 8,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: activeStep >= 4 ? Colors.orange : Colors.white,
+              ),
+            ),
+            title: 'Delivered',
+          ),
+        ],
+        onStepReached: (index) => setState(() => activeStep = index),
+      ),
+    );
   }
 }
