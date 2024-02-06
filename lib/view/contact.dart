aa10c646efc2e940120364803e47895e695bafb5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-06 14:29:25 +0100

    ModifiedM lib/view/contact.dart

---
+++ lib/view/contact.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../shared/globals.dart';
+
+class Contact extends StatefulWidget {
+  const Contact({super.key});
+
+  @override
+  State<Contact> createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 48, horizontal: 64),
+      color: evenDarkBgColor,
+      child: Row(
+        children: <Widget>[
+          Text("Contact", style: GoogleFonts.jura(fontSize: 64, color: whiteColor, fontWeight: FontWeight.w500)),
+          const Spacer(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                "If you require any further information, please feel free to reach out to me. If I can be of any help, please do not hesitate to contact me. Please let me know if you have further questions on this matter.",
+                style: GoogleFonts.jura(fontSize: 18, color: whiteColor, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(hintText: "Name", hintStyle: GoogleFonts.jura(fontSize: 18, color: whiteColor, fontWeight: FontWeight.w500), fillColor: evenDarkBgColor),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
