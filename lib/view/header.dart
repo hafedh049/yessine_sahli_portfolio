8ee482d0b4659cd463b5caab0238af0049b1d717
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-07 14:44:59 +0100

    ModifiedM lib/view/header.dart

--- lib/view/header.dart
+++ lib/view/header.dart
@@ -22,7 +22,7 @@ class _HeaderState extends State<Header> {
     return AnimatedContainer(
       duration: 100.ms,
       width: MediaQuery.sizeOf(context).width,
-      height: MediaQuery.sizeOf(context).height * .1,
+      height: MediaQuery.sizeOf(context).height * .11,
       padding: const EdgeInsets.all(24),
       decoration: BoxDecoration(color: headerBgColor, boxShadow: <BoxShadow>[BoxShadow(blurStyle: BlurStyle.outer, color: whiteColor.withOpacity(.6), offset: const Offset(0, 1))]),
       child: Column(
@@ -45,7 +45,14 @@ class _HeaderState extends State<Header> {
                     highlightColor: transparent,
                     onTap: () => true,
                     onHover: (bool value) => _(() => _titleState = value),
-                    child: Text("Personal Website | Yassine Sahli", style: GoogleFonts.jura(fontSize: 22, color: _titleState ? lightBlueColor : whiteColor, fontWeight: FontWeight.w500)),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Text(
+                        "Personal Website | Yassine Sahli",
+                        style: GoogleFonts.jura(fontSize: 20, color: _titleState ? lightBlueColor : whiteColor, fontWeight: FontWeight.w500),
+                        overflow: TextOverflow.fade,
+                      ),
+                    ),
                   );
                 },
               ),
@@ -71,7 +78,7 @@ class _HeaderState extends State<Header> {
                                           splashColor: transparent,
                                           highlightColor: transparent,
                                           onTap: () async {
-                                            controller.animateTo(MediaQuery.sizeOf(context).height * _sections.indexOf(section_) + MediaQuery.sizeOf(context).height * .1, duration: 300.milliseconds, curve: Curves.linear);
+                                            controller.animateTo(MediaQuery.sizeOf(context).height * _sections.indexOf(section_) + MediaQuery.sizeOf(context).height * .11, duration: 300.milliseconds, curve: Curves.linear);
                                             _(() => section = section_);
                                           },
                                           child: Text(section_, style: GoogleFonts.jura(fontSize: 16, color: section_ == section ? lightBlueColor : whiteColor, fontWeight: FontWeight.w500)),
@@ -97,7 +104,7 @@ class _HeaderState extends State<Header> {
               const Spacer(),
               LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints constraints) {
-                  return window.innerWidth! > 900 || window.innerWidth! <= 400
+                  return window.innerWidth! > 900 || window.innerWidth! <= 600
                       ? const SizedBox()
                       : Row(
                           mainAxisSize: MainAxisSize.min,
@@ -108,6 +115,51 @@ class _HeaderState extends State<Header> {
                         );
                 },
               ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  StatefulBuilder(
+                    builder: (BuildContext context, void Function(void Function()) _) {
+                      return Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          for (final String section_ in _sections) ...<Widget>[
+                            AnimatedScale(
+                              duration: 500.ms,
+                              scale: section_ == section ? 1.01 : 1,
+                              child: InkWell(
+                                hoverColor: transparent,
+                                splashColor: transparent,
+                                highlightColor: transparent,
+                                onTap: () async {
+                                  controller.animateTo(MediaQuery.sizeOf(context).height * _sections.indexOf(section_) + MediaQuery.sizeOf(context).height * .11, duration: 300.milliseconds, curve: Curves.linear);
+                                  _(() => section = section_);
+                                },
+                                child: Text(section_, style: GoogleFonts.jura(fontSize: 16, color: section_ == section ? lightBlueColor : whiteColor, fontWeight: FontWeight.w500)),
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                          ],
+                        ],
+                      );
+                    },
+                  ),
+                  InkWell(
+                    hoverColor: transparent,
+                    splashColor: transparent,
+                    highlightColor: transparent,
+                    onTap: () async => await launchUrlString("http://www.google.com"),
+                    child: Text("CV", style: GoogleFonts.jura(fontSize: 16, color: whiteColor, fontWeight: FontWeight.w500)),
+                  ),
+                ],
+              )
             ],
           ),
         ],
