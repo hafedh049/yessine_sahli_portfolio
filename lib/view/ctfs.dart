2aa8cbf5a9c11be4f1952dc6912ff3ce8deb4606
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-02-07 10:24:44 +0100

    ModifiedM lib/view/ctfs.dart

--- lib/view/ctfs.dart
+++ lib/view/ctfs.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../shared/globals.dart';
 
 class CTFs extends StatefulWidget {
   const CTFs({super.key});
@@ -8,8 +11,30 @@ class CTFs extends StatefulWidget {
 }
 
 class _CTFsState extends State<CTFs> {
+  final List<Map<String, dynamic>> _items = <Map<String, dynamic>>[];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: oddDarkBgColor,
+      alignment: Alignment.center,
+      constraints: BoxConstraints(minHeight: MediaQuery.sizeOf(context).height),
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      child: Wrap(
+        alignment: WrapAlignment.center,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        runAlignment: WrapAlignment.center,
+        runSpacing: 20,
+        spacing: 20,
+        children: <Widget>[
+          for (final Map<String, dynamic> item in _items)
+            AnimatedContainer(
+              duration: 300.ms,
+              child: Column(
+                children: <Widget>[],
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
